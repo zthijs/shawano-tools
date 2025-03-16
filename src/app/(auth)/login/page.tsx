fa15e00f6caeb7d380cbece44a92ba3d111f5405
zthijs <zthijs@outlook.com>
2025-03-16 12:13:33 +0100

    update login

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,4 +1,4 @@
-import { GalleryVerticalEnd } from "lucide-react";
+import { ShawanosLogo } from "@/components/shared/shawanos";
 import Link from "next/link";
 import { Suspense } from "react";
 import { LoginButton } from "./button";
@@ -9,10 +9,10 @@ export default function LoginPage() {
 			<div className="flex flex-col gap-4 p-6 md:p-10">
 				<div className="flex justify-center gap-2 md:justify-start">
 					<Link href="/" className="flex items-center gap-2 font-medium">
-						<div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
-							<GalleryVerticalEnd className="size-4" />
+						<div className="flex h-6 w-6 items-center justify-center rounded-md text-primary-foreground">
+							<ShawanosLogo />
 						</div>
-						Shawano&apos;s
+						Shawano&apos;s PR Tools
 					</Link>
 				</div>
 				<div className="flex flex-1 items-center justify-center">
